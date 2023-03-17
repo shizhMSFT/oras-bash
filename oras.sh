f46270d3f64da88eaaf2ccfc97d23860179c0434
Shiwei Zhang <shizh@microsoft.com>
2023-03-18 00:40:25 +0800

    feat: test chunked upload
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

--- oras.sh
+++ oras.sh
@@ -119,6 +119,75 @@ function oras_repo_tags() {
     $curl -Ls "$scheme://$reg/v2/$repo/tags/list" | jq -r '.tags[]' | sort
 }
 
+function oras_test() {
+    local cmd=$1
+    shift
+    case $cmd in
+        chunked-upload)
+            oras_test_chunked_upload "$@"
+            ;;
+        *)
+            echo "Unknown command: $cmd"
+            exit 1
+            ;;
+    esac
+}
+
+function oras_test_chunked_upload() {
+    echo "---------------------------"
+    echo "Testing blob chunked upload"
+    echo "---------------------------"
+
+    local ref=$1
+    local reg=${ref%%/*}
+    local repo=${ref#*/}
+    local scheme=$(get_scheme "$reg")
+    local file=$2
+    local digest="sha256:$(sha256sum "$file" | cut -d" " -f1 | tr -d "\r")"
+    echo "$file: digest: $digest: size: $(stat -c%s "$file")"
+
+    # start an upload
+    printf "\e[31m%s\e[0m\n" ">>> start upload"
+    resp=$($curl -s "$scheme://$reg/v2/$repo/blobs/uploads/" -XPOST -I)
+    local upload_url=$(echo "$resp" | grep -i location | cut -d" " -f2 | tr -d "\r")
+    if [[ $upload_url == /* ]]; then
+        upload_url="$scheme://$reg$upload_url"
+    fi
+    local uuid=$(echo "$resp" | grep -i docker-upload-uuid | cut -d" " -f2 | tr -d "\r")
+    echo "UUID: $uuid"
+
+    # chunked upload
+    printf "\e[31m%s\e[0m\n" ">>> chunked upload"
+    echo "Location: $upload_url"
+    resp=$($curl -XPATCH -H "Content-Type: application/octet-stream" --data-binary "@$file" "$upload_url" -sD-)
+    upload_url=$(echo "$resp" | grep -i location | cut -d" " -f2 | tr -d "\r")
+        if [[ $upload_url == /* ]]; then
+        upload_url="$scheme://$reg$upload_url"
+    fi
+
+    printf "\e[31m%s\e[0m\n" ">>> upload status"
+    printf "\e[33m%s\e[0m\n" ">>> try /v2/$repo/blobs/uploads/$uuid"
+    $curl "$scheme://$reg/v2/$repo/blobs/uploads/$uuid" -D-
+    printf "\e[33m%s\e[0m\n" ">>> try $upload_url"
+    $curl "$upload_url" -D-
+
+    # commit
+    printf "\e[31m%s\e[0m\n" ">>> commit"
+    $curl -XPUT -H "Content-Type: application/octet-stream" "$upload_url&digest=$digest" -D-
+}
+
+function usage() {
+    echo "Usage: $0 <command> [args]"
+    echo "Commands:"
+    echo "  blob fetch <ref> <file>"
+    echo "  blob push <ref> <file>"
+    echo "  manifest fetch <ref>"
+    echo "  repo ls <registry>"
+    echo "  repo tags <ref>"
+    echo "  test chunked-upload <ref> <file>"
+    echo "  version"
+}
+
 function get_scheme() {
     local reg=$1
     if [[ $reg == localhost:* ]]; then
@@ -134,7 +203,7 @@ if [ -n "$ORAS_AUTH" ]; then
 fi
 
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 <command> [args]"
+    usage
     exit 1
 fi
 
@@ -149,6 +218,12 @@ case $cmd in
         ;;
     repo)
         oras_repo "$@"
+        ;;
+    test)
+        oras_test "$@"
+        ;;
+    version)
+        echo "oras.sh v0.1.0"
         ;;
     *)
         echo "Unknown command: $cmd"
